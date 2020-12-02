4834d00e41bb5da854c5c2f7f8bc72a06041ee33
Francis Begyn <francis@begyn.be>
2020-12-02 07:20:50 +0100

    day 02 - solution

---
+++ go/cmd/day02/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fbegyn/aoc2020/go/helpers"
+	"bufio"
+	"strings"
+	"strconv"
+	"log"
+	"regexp"
+)
+
+type PasswordRule struct {
+	limits []int
+	letter string
+}
+
+func PasswordRuleFromLine(line string) (PasswordRule, string) {
+	set1 := strings.Split(line, ":")
+	set2 := strings.Split(set1[0], " ")
+	set3 := strings.Split(set2[0], "-")
+
+	lower, err := strconv.Atoi(set3[0])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	upper, err := strconv.Atoi(set3[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return PasswordRule{
+		limits: []int{lower, upper},
+		letter: set2[1],
+	}, set1[1]
+}
+
+// regex := regexp.MustCompile("(\\d+)-(\\d+) ([a-z]): ([a-z]+)$")
+func PasswordRuleFromLineRegex(line string, regex *regexp.Regexp) (PasswordRule, string) {
+	matches := regex.FindAllStringSubmatch(line, -1)
+	if matches == nil {
+		return PasswordRule{}, ""
+	}
+
+	lower, err := strconv.Atoi(matches[0][1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	upper, err := strconv.Atoi(matches[0][2])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return PasswordRule{
+		limits: []int{lower, upper},
+		letter: matches[0][3],
+	}, matches[0][4]
+}
+
+func (r *PasswordRule) ValidSled(p string) bool {
+	count := strings.Count(p, r.letter)
+	if count < r.limits[0] || r.limits[1] < count {
+		return false
+	}
+	return true
+}
+
+func (r *PasswordRule) ValidToboggan(p string) bool {
+	count := 0
+	for _, v := range r.limits {
+		if p[v-1] == r.letter[0] {
+			count += 1
+		}
+	}
+	if count != 1 {
+		return false
+	}
+	return true
+}
+
+func main() {
+	input := helpers.OpenFile("../inputs/day02/input.txt")
+	defer input.Close()
+
+	validSledPassword := 0
+	validTobogganPassword := 0
+
+	//regex := regexp.MustCompile("(\\d+)-(\\d+) ([a-z]): ([a-z]+)$")
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		rule, password := PasswordRuleFromLine(line)
+		//r, p := PasswordRuleFromLineRegex(line, regex)
+
+		if rule.ValidSled(password) {
+			validSledPassword += 1
+		}
+
+		if rule.ValidToboggan(password) {
+			validTobogganPassword += 1
+		}
+	}
+
+	fmt.Println(validSledPassword)
+	fmt.Println(validTobogganPassword)
+}
+
+func part1() (error) {
+	return nil
+}
+
+func part2() (error) {
+	return nil
+}
