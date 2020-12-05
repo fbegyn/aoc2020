29c36a2494b6397b90733e4157395f086abe82b6
Francis Begyn <francis@begyn.be>
2020-12-05 10:52:40 +0100

    day 05 - benchmarks

--- go/cmd/day05/main_test.go
+++ go/cmd/day05/main_test.go
@@ -4,10 +4,11 @@ import (
 	"bufio"
 	"testing"
 
-	"strings"
 	"sort"
-	"github.com/fbegyn/aoc2020/go/helpers"
 	"strconv"
+	"strings"
+
+	"github.com/fbegyn/aoc2020/go/helpers"
 )
 
 func BenchmarkPart1(b *testing.B) {
@@ -15,24 +16,25 @@ func BenchmarkPart1(b *testing.B) {
 	defer input.Close()
 
 	ids := []int{}
-	
-	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		line := scanner.Text()
-		line = strings.ReplaceAll(line, "F", "0")
-		line = strings.ReplaceAll(line, "B", "1")
-		line = strings.ReplaceAll(line, "L", "0")
-		line = strings.ReplaceAll(line, "R", "1")
-		ID, err := strconv.ParseInt(line, 2, 32)
-		if err != nil {
-			b.Fatalf("failed to parse ID: %v", err)
-		}
-		ids = append(ids, int(ID))
-	}
 
-	// part 1
-	sort.Ints(ids)
-	_ = ids[len(ids)-1]
+	scanner := bufio.NewScanner(input)
+	for i := 0; i < b.N; i++ {
+		for scanner.Scan() {
+			line := scanner.Text()
+			line = strings.ReplaceAll(line, "F", "0")
+			line = strings.ReplaceAll(line, "B", "1")
+			line = strings.ReplaceAll(line, "L", "0")
+			line = strings.ReplaceAll(line, "R", "1")
+			ID, err := strconv.ParseInt(line, 2, 32)
+			if err != nil {
+				b.Fatalf("failed to parse ID: %v", err)
+			}
+			ids = append(ids, int(ID))
+		}
+
+		// part 1
+		sort.Ints(ids)
+	}
 }
 
 func BenchmarkPart2(b *testing.B) {
@@ -40,29 +42,31 @@ func BenchmarkPart2(b *testing.B) {
 	defer input.Close()
 
 	ids := []int{}
-	
+
 	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		line := scanner.Text()
-		line = strings.ReplaceAll(line, "F", "0")
-		line = strings.ReplaceAll(line, "B", "1")
-		line = strings.ReplaceAll(line, "L", "0")
-		line = strings.ReplaceAll(line, "R", "1")
-		ID, err := strconv.ParseInt(line, 2, 32)
-		if err != nil {
-			b.Fatalf("failed to parse ID: %v", err)
+	for i := 0; i < b.N; i++ {
+		for scanner.Scan() {
+			line := scanner.Text()
+			line = strings.ReplaceAll(line, "F", "0")
+			line = strings.ReplaceAll(line, "B", "1")
+			line = strings.ReplaceAll(line, "L", "0")
+			line = strings.ReplaceAll(line, "R", "1")
+			ID, err := strconv.ParseInt(line, 2, 32)
+			if err != nil {
+				b.Fatalf("failed to parse ID: %v", err)
+			}
+			ids = append(ids, int(ID))
 		}
-		ids = append(ids, int(ID))
-	}
 
-	// part 1
-	sort.Ints(ids)
+		// part 1
+		sort.Ints(ids)
 
-	// part 2
-	var myseat int
-	for ind := range ids {
-		if myseat =ids[ind]+1; myseat != ids[ind+1] {
-			break
+		// part 2
+		var myseat int
+		for ind := range ids {
+			if myseat = ids[ind] + 1; myseat != ids[ind+1] {
+				break
+			}
 		}
 	}
 }
