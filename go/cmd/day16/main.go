1ee2a1c11f1a60f0a4bfd65ddc713cf005680380
Francis Begyn <francis@begyn.be>
2020-12-16 17:47:02 +0100

    day 16 - solve part 2

--- go/cmd/day16/main.go
+++ go/cmd/day16/main.go
@@ -99,14 +99,10 @@ func main() {
 		}
 	}
 
-	mapping := make(map[int]string)
+	candidates := make(map[string][]int)
 	for rule, bounds := range validBounds {
 		var count int
 		for col := range nearbyTicket[0] {
-			_, mapped := mapping[col]
-			if mapped {
-				continue
-			}
 			count = 0
 			for _, row := range nearbyTicket {
 				lower := CheckIntRange(row[col], bounds[0])
@@ -116,24 +112,67 @@ func main() {
 				}
 			}
 			if count == len(nearbyTicket) {
-				mapping[col] = rule
+				candidates[rule] = append(candidates[rule], col)
+			}
+		}
+	}
+
+	// filter through the candidates
+	// work means we aren't done yet
+	work := true
+	for work {
+		// go through each rule and it's possible candidates
+		for rule, candids := range candidates {
+			// if there is more than 1 candidate, we need to filter more before
+			// processing this ruleset. There also needs to be a candidate for
+			// every rule
+			if len(candids) > 1 {
+				continue
+			} else if len(candids) == 0 {
+				panic("oops, no candidates found")
+			}
+
+			// if there is only 1 candidate, select it for processing
+			correct := candids[0]
+			// iterate through the other ruleset to filter out the current
+			for k, v := range candidates {
+				// skip rulesets with 1 candidate and of course, the
+				// current ruleset
+				if len(v) == 1 || k == rule {
+					continue
+				}
+				// filter out the current col from the other candidates
+				for i, c := range v {
+					if c == correct {
+						candidates[k] = append(v[:i],v[i+1:]...)
+						break
+					}
+				}
+			}
+		}
+		// if we are here, we assume no work is done
+		work = false
+		// if there is a ruleset with more than 1 candidate we need to
+		// start work again untill all rules are filtered
+		for _, c := range candidates {
+			if len(c) > 1 {
+				work = true
 				break
 			}
 		}
 	}
 
 	depart := []int{}
-	for k, v := range mapping {
-		if strings.HasPrefix(v, "departure") {
-			fmt.Printf("%s is col %d  and has value %d\n", v, k, ownTicket[k])
-			depart = append(depart, ownTicket[k])
+	for k, v := range candidates {
+		if strings.HasPrefix(k, "departure") {
+			depart = append(depart, v[0])
 		}
 	}
-
-	mult := depart[0]
-	for _, n := range depart[1:] {
-		mult *= n
-	}
+	
+	mult := ownTicket[depart[0]]
+        for _, n := range depart[1:] {
+                mult *= ownTicket[n]
+        }
 
 	fmt.Printf("solution for part 1: %d\n", helpers.SumOfIntArray(invalidNumbers))
 	fmt.Printf("solution for part 2: %d\n", mult)
