e6ac3149b15c9a5dac4b40d36e874286cccc4394
Francis Begyn <francis@begyn.be>
2020-12-18 22:45:19 +0100

    day 18 - part 2 solved

--- go/cmd/day18/main.go
+++ go/cmd/day18/main.go
@@ -40,9 +40,56 @@ L:
 			if op == '+' {
 				result += number
 			} else {
+
 				result *= number
 			}
 		default:
+			ind++
+			break L
+		}
+	}
+	return result, ind
+}
+
+func eval2(s string, ind int, mult bool) (int, int) {
+	result := 0
+	op := '+'
+L:
+	for ind < len(s) {
+		switch ch := s[ind]; ch {
+		case ' ':
+			ind++
+		case '+':
+			op = '+'
+			ind++
+		case '*':
+			op = '*'
+			ind++
+			r, j := eval2(s, ind, true)
+			ind = j
+			result *= r
+		case '(':
+			ind++
+			r, j := eval2(s, ind, false)
+			ind = j
+			if op == '+' {
+				result += r
+			} else {
+				result *= r
+			}
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			number := int(ch - '0')
+			if op == '+' {
+				result += number
+			} else {
+
+				result *= number
+			}
+			ind++
+		default:
+			if mult {
+				break L
+			}
 			ind++
 			break L
 		}
@@ -56,10 +103,14 @@ func main() {
 	go helpers.StreamLines(file, input)
 
 	results := []int{}
+	results2 := []int{}
 	for inp := range input {
 		res, _ := eval(inp, 0)
+		res2, _ := eval2(inp, 0, true)
 		results = append(results, res)
+		results2 = append(results2, res2)
 	}
 
 	fmt.Printf("solution to part 1: %d\n", helpers.SumOfIntArray(results))
+	fmt.Printf("solution to part 2: %d\n", helpers.SumOfIntArray(results2))
 }
--- go/cmd/day18/main_test.go
+++ go/cmd/day18/main_test.go
@@ -51,6 +51,53 @@ func TestEval(t *testing.T) {
 	}
 }
 
+func TestEval2(t *testing.T) {
+	cases := []struct{
+		name string
+		inp string
+		start int
+		want int
+	}{
+		{
+			"test-1", "1 + 2 * 3 + 4 * 5 + 6",
+			0, 231,
+		},
+		{
+			"test-2", "1 + (2 * 3) + (4 * (5 + 6))",
+			0, 51,
+		},
+		{
+			"test-3", "2 * 3 + (4 * 5",
+			0, 46,
+		},
+		{
+			"test-4", "5 + (8 * 3 + 9 + 3 * 4 * 3)",
+			0, 1445,
+		},
+		{
+			"test-5", "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
+			0, 669060,
+		},
+		{
+			"test-6-pre", "0 + ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2",
+			0, 23340,
+		},
+		{
+			"test-6", "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2",
+			0, 23340,
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.name, func(t *testing.T){
+			res, _ := eval2(test.inp, test.start, true)
+			if res != test.want {
+				t.Fatalf("eval returned %d, but expected %d\n", res, test.want)
+			}
+		})
+	}
+}
+
 func BenchmarkPart1(b *testing.B) {
 	//file := "../../../inputs/day18/input.txt"
 	for i := 0; i < b.N; i++ {
